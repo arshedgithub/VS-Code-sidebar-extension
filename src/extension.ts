debcbcb8ffa3c7b4ea3adb505fb5ebabb85c7d44
arshedgithub <arshedahmedmail@gmail.com>
2025-01-23 09:38:57 +0530

    removing consoles

--- src/extension.ts
+++ src/extension.ts
@@ -7,12 +7,10 @@ export function activate(context: vscode.ExtensionContext) {
 
   class MyTreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
     getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
-      console.log('getTreeItem called:', element.label);
       return element;
     }
   
     getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
-      console.log('getChildren called:', element ? element.label : 'root');
       if (element) {
         return Promise.resolve([]);
       } else {
