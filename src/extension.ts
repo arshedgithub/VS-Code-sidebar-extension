1f88b87d40e100eeb713964189dc49b79650ac02
arshedgithub <arshedahmedmail@gmail.com>
2025-01-23 09:35:43 +0530

    fixing bugs

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,26 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  const myProvider = new MyTreeDataProvider();
-  vscode.window.registerTreeDataProvider('myView', myProvider);
-}
-
-class MyTreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
-  getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
-    return element;
+    const myTreeDataProvider = new MyTreeDataProvider();
+    vscode.window.registerTreeDataProvider('myView', myTreeDataProvider);
   }
 
-  getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
-    if (element) {
-      return Promise.resolve([]);
-    } else {
-      return Promise.resolve([
-        new vscode.TreeItem('Item 1'),
-        new vscode.TreeItem('Item 2'),
-        new vscode.TreeItem('Item 3')
-      ]);
+  class MyTreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+    getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+      console.log('getTreeItem called:', element.label);
+      return element;
+    }
+  
+    getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
+      console.log('getChildren called:', element ? element.label : 'root');
+      if (element) {
+        return Promise.resolve([]);
+      } else {
+        return Promise.resolve([
+          new vscode.TreeItem('Item 1'),
+          new vscode.TreeItem('Item 2'),
+          new vscode.TreeItem('Item 3')
+        ]);
+      }
     }
   }
-}
